b1ca4682f4bc2282d85c3aac2ea537583312d479
ROSotto <RyanOSotto@Gmail.com>
2018-10-12 22:20:45 -0500

    Scrolling animation was added

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
-    $('h1').click(function(){
-        $(this).css('background-color','#ff0000');
-    })
-
+    /* For the sticky navigation */
+    $('.js--section-features').waypoint(function(direction){
+          if (direction == "down") {
+            $('nav').addClass('sticky');
+          } else {
+            $('nav').removeClass('sticky');
+          } 
+        }, { 
+            offset: '60px;'
+      });
+    /*------scroll on buttons----*/   
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate( {scrollTop: $('.js--section-plans').offset().top}, 1000);
+     });  
+     $('.js--scroll-to-start').click(function () {
+      $('html, body').animate( {scrollTop: $('.js--section-features').offset().top}, 1000);
+     }); 
 });
+
